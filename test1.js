1d913d9088fa45e5181befd6059ae0c64e1d9aef
Zeb <bfkjz@outlook.com>
2024-12-11 17:38:58 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -5,16 +5,14 @@ if (!requestScreenCapture()) {
     toast("请求截图失败");
     exit();
 }
-
-
 let bimg = captureScreen()       //bimg就需截取手机屏幕。。意思就是截屏
 let simg = images.read("/storage/emulated/0/Pictures/Screenshots/1.png");//括号里是图片地址。。
 let p = findImage(bimg, simg);
 if (p) {
-    log("找到啦);
+    log("找到啦");
     log(p);
 
 } else {
-    log("没找到);
+    log("没找到");
 }
  
